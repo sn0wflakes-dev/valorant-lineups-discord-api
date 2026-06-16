d62c4b7cda6fb3f932aec3d5fb107e920de56a88
Sn0wflakes <abdurrahmanali.dev@gmail.com>
2026-06-16 21:14:50 +0000

    init: initialization api projects using java springboot 4.0.7

---
+++ src/main/java/sn0w/discord/api/ValorantDiscordBotApi.java
@@ -0,0 +1,13 @@
+package sn0w.discord.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValorantDiscordBotApi {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValorantDiscordBotApi.class, args);
+    }
+
+}
